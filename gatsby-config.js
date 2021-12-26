00dfd438fa1824ad4fbc9f646f2640225de1edd7
Amir Hamada <amir.hamada@live.fr>
2021-12-26 22:22:15 +0100

    add siteMetadata in gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title:'Polytech international',
+    description:"website of polytech international university",
+    author:"@AmirHamada,@SyrineHamada"
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
